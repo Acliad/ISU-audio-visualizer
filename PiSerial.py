b7436040443cecf25e6d333dc0a90e05354cfd43
Zach Kauffman <ztk@iastate.edu>
2018-02-04 21:52:07 -0600

    restructured the python code to use 2d list

--- PiSerial.py
+++ PiSerial.py
@@ -6,9 +6,13 @@ port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
 colors = []
 
 for c in range(5):
-    colors.append(random.randint(0, 16777215))
+    colorRGB = []
+    colorRGB.append(random.randint(0, 255)) #R
+    colorRGB.append(random.randint(0, 255)) #G
+    colorRGB.append(random.randint(0, 255)) #B
 
 for c in colors:
-    port.write(123)
-    rcv = port.read(1)
-    print(rcv)
+    for rgb in c:
+        port.write(rgb);
+        rcv = port.read(1);
+        pritn(rcv)
